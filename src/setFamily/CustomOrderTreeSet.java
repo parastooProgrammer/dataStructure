244070cf722883627cc6a92c66dbe91ced4bb2e1
Administrator <parastoo.sa.1982@gmail.com>
2025-04-03 23:10:54 -0400

    CustomOrderTreeSet

--- src/setFamily/CustomOrderTreeSet.java
+++ src/setFamily/CustomOrderTreeSet.java
@@ -4,7 +4,18 @@ import java.util.Comparator;
 import java.util.TreeSet;
 
 public class CustomOrderTreeSet<T> {
+    /*
+           10
+      /  \
+     5    20
+         /   \
+       15     25
+Binary Search Tree (BST) Structure – Every left child is smaller, and every right child is larger.
 
+Self-Balancing (Red-Black Tree) – Ensures O(log n) operations.
+
+No Duplicates – A TreeSet does not allow duplicate elements.
+     */
     TreeSet<T> treeSet;
 
 
